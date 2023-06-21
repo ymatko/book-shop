d381c9ab891e97822e02357522ed12da1eebb5df
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-06-21 08:31:51 +0200

    Add edit and delete action and notification

--- BookShop/Controllers/CategoryController.cs
+++ BookShop/Controllers/CategoryController.cs
@@ -20,7 +20,7 @@ namespace BookShop.Controllers
         {
             return View();
         }
-        [HttpPost]
+		[HttpPost]
 		public IActionResult Create(Category obj)
 		{
             if (obj.Name == obj.DisplayOrder.ToString())
@@ -31,9 +31,65 @@ namespace BookShop.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+				TempData["success"] = "Category created successfully";
 				return RedirectToAction("Index");
 			}
             return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+			if(id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);
+			//Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();
+
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if(obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
 		}
 	}
 }
