d2e72e20a588a81144c029bb7b9b2cfd9ed75fc6
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-06-21 13:55:57 +0200

    Add button edit category

---
+++ BookShopRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookShopRazor_Temp.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
