b4d8ad8fe90dd344580d9a57d6ea77fb418a8ca3
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-06-28 21:01:27 +0200

    Add Foreign Key in EF Core

--- Shop.Models/Product.cs
+++ Shop.Models/Product.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Shop.Models
 {
@@ -29,6 +30,7 @@ namespace Shop.Models
 		[Range(1, 1000)]
 		public double Price { get; set; }
 
+
 		[Required]
 		[Display(Name = "Price for 50+")]
 		[Range(1, 1000)]
@@ -38,5 +40,9 @@ namespace Shop.Models
 		[Display(Name = "Price for 100+")]
 		[Range(1, 1000)]
 		public double Price100 { get; set; }
+
+		public int CategoryId { get; set; }
+		[ForeignKey("CategoryId")]
+		public Category Category { get; set; }
 	}
 }
