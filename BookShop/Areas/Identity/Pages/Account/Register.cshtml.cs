37cbed7cdcb7e422bf247b1ec411c639ba6ecaa1
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-07-05 12:03:32 +0200

    Create Roles in Database

--- BookShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,12 +19,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Shop.Models;
+using Shop.Utility;
 
 namespace BookShop.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;    
         private readonly UserManager<IdentityUser> _userManager;
         private readonly IUserStore<IdentityUser> _userStore;
         private readonly IUserEmailStore<IdentityUser> _emailStore;
@@ -33,11 +35,13 @@ namespace BookShop.Areas.Identity.Pages.Account
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
+            _roleManager = roleManager;
             _userManager = userManager;
             _userStore = userStore;
             _emailStore = GetEmailStore();
@@ -103,7 +107,14 @@ namespace BookShop.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            ReturnUrl = returnUrl;
+            if(!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult()){
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+				_roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+				_roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+				_roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+			}
+
+			ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
--- Shop.Utility/SD.cs
+++ Shop.Utility/SD.cs
@@ -8,5 +8,9 @@ namespace Shop.Utility
 {
 	public static class SD
 	{
+		public const string Role_Customer = "Costumer";
+		public const string Role_Company = "Company";
+		public const string Role_Admin = "Admin";
+		public const string Role_Employee = "Employee";
 	}
 }
