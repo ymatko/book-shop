c1113eb29f273cfd49e9e4c9eb730e6fbaf6f544
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-07-06 20:16:45 +0200

    Add Company Table and Company in UnitOfWork

---
+++ Shop.DataAccess/Repository/IRepository/CompanyRepository.cs
@@ -0,0 +1,25 @@
+﻿using Shop.DataAccess.Data;
+using Shop.Models;
+using Shop.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataAccess.Repository
+{
+	public class CompanyRepository : Repository<Company>, ICompanyRepository
+	{
+		private ApplicationDbContext _db;
+		public CompanyRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+		public void Update(Company obj)
+		{
+			_db.Companies.Update(obj);
+		}
+	}
+}
