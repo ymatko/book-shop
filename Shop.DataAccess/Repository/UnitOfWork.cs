1c35db2e80c026420f3eb19ba054c81489ca3300
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-07-07 08:58:43 +0200

    Add Shopping Cart to Repository

--- Shop.DataAccess/Repository/UnitOfWork.cs
+++ Shop.DataAccess/Repository/UnitOfWork.cs
@@ -15,6 +15,7 @@ namespace Shop.DataAccess.Repository
 		public ICategoryRepository Category { get; private set; }
 		public IProductRepository Product { get; private set; }
 		public ICompanyRepository Company { get; private set; }
+		public IShoppingCartRepository ShoppingCart { get; private set; }
 
 		public UnitOfWork(ApplicationDbContext db)
 		{
@@ -22,6 +23,7 @@ namespace Shop.DataAccess.Repository
 			Category = new CategoryRepository(_db);
 			Product = new ProductRepository(_db);
 			Company = new CompanyRepository(_db);
+			ShoppingCart = new ShoppingCartRepository(_db);
 		}
 		public void Save()
 		{
