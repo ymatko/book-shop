973dea9a24ed81ca2ef21c36b3ab0ba709a5e74c
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-07-08 18:34:59 +0200

    Create order header and details model

--- Shop.DataAccess/Data/ApplicationDbContext.cs
+++ Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace Shop.DataAccess.Data
 		public DbSet<ApplicationUser> applicationUsers { get; set; }
 		public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+		public DbSet<OrderHeader> OrderHeaders { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
