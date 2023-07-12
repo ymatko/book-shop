b1e6c108dcfa45c7b37c65199fe66cdb1e5d4e6f
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-07-12 21:20:49 +0200

    Add OrderVM and Order List UI

---
+++ BookShop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Shop.DataAccess.Repository.IRepository;
+using Shop.Models;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+		#endregion
+	}
+}
