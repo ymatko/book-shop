3118bd0fe9cc7c41ba715a87c7746716a60d2e09
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-07-18 09:52:10 +0200

    Add session in app

--- BookShop/Areas/Customer/Controllers/HomeController.cs
+++ BookShop/Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using Shop.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using Shop.Utility;
+using Microsoft.AspNetCore.Http;
 
 namespace BookShop.Areas.Customer.Controllers
 {
@@ -51,14 +53,17 @@ namespace BookShop.Areas.Customer.Controllers
                 //shopping cart exists
                 cartFromDb.Count += shoppingCart.Count;
                 _unitOfWork.ShoppingCart.Update(cartFromDb);
-            }
-            else
+				_unitOfWork.Save();
+			}
+			else
             {
                 //add cart record
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
+				_unitOfWork.Save();
+
+				HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
             }
             TempData["success"] = "Cart update successfully";
-            _unitOfWork.Save();
 
             return RedirectToAction(nameof(Index));
         }
