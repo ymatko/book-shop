2774a2cf45cb1e55a1b1168508af33f4fe905801
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-07-21 21:13:37 +0200

    Lock and unlock user action

--- BookShop/Areas/Admin/Controllers/UserController.cs
+++ BookShop/Areas/Admin/Controllers/UserController.cs
@@ -34,14 +34,45 @@ namespace BookShop.Areas.Admin.Controllers
 		public IActionResult GetAll()
 		{
 			List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u=>u.Company).ToList();
-			
+
+			var userRoles = _db.UserRoles.ToList();
+			var roles = _db.Roles.ToList();
+
+			foreach(var user in objUserList)
+			{
+				var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+				user.Role = roles.FirstOrDefault(u => u.Id ==  roleId).Name;
+
+
+				if(user.Company == null)
+				{
+					user.Company = new Company { Name = "" };
+				}
+			}
 			return Json( new { data = objUserList });
 		}
 
-		[HttpDelete]
-		public IActionResult Delete(int? id)
+		[HttpPost]
+		public IActionResult LockUnlock([FromBody]string id)
 		{
-			return Json(new { success = true, message = "Delete Successful" });
+			var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+			if (objFromDb == null)
+			{
+				return Json(new { success = false, message = "Error while Locking/Unlocking" });
+			}
+
+			if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+			{
+				//user is currently locked and we need to unlock them
+				objFromDb.LockoutEnd = DateTime.Now;
+			}
+			else
+			{
+				objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+			}
+			_db.SaveChanges();
+
+			return Json(new { success = true, message = "Operation Successful" });
 		}
 
 		#endregion
