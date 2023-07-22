4abdedc63635c919d7b763b2edd3d167b349bb1e
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-07-22 21:33:05 +0200

    Add upload more image to product

--- Shop.DataAccess/Repository/ProductRepository.cs
+++ Shop.DataAccess/Repository/ProductRepository.cs
@@ -32,6 +32,7 @@ namespace Shop.DataAccess.Repository
                 objFromDb.Description = obj.Description;
                 objFromDb.CategoryId = obj.CategoryId;
                 objFromDb.Author = obj.Author;
+                objFromDb.ProductImages = obj.ProductImages;
                 //if (obj.ImageUrl != null)
                 //{
                 //    objFromDb.ImageUrl = obj.ImageUrl;
